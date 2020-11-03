98ab4b5c6c369f2a21d19fa31653d6b7a8a4748c
nemota <walkinggreencoordinator@gmail.com>
2020-11-03 22:53:47 +0900

    形式の大幅変更、JSONの更新

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
-document.getElementById("load").onclick = function () {
-	document.getElementById("load").disabled=true;
-	var xhr=new XMLHttpRequest();
-	xhr.onreadystatechange = function () {
-		if(xhr.readyState==4){
-			if(xhr.status==200){
-				show(xhr.responseText);
-			}else{
-				alert("有効なファイルを指定してね");
-			}
-		}
-	
-	}
-	xhr.open("GET",document.getElementById("path").value.trim());
-	xhr.send(null);
-}
-function show(res){
-	var list=JSON.parse(res);
+$.getJSON("data.json",function (json) {
+	var list=json;
 	var table=document.getElementById("table");
 	console.log(list);
 	var showlist=["name","source","flags","replace"];
@@ -28,4 +12,4 @@ function show(res){
 		}
 		table.appendChild(tr);
 	}
-}
+})
